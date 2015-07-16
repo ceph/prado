cf602f0f637c1980b5a261136ab80c2bc80d179b
Alfredo Deza <alfredo@deza.pe>
2015-07-16 11:17:41 -0400

    working build controller
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- webservice/prado/controllers/build.py
+++ webservice/prado/controllers/build.py
@@ -1,17 +1,43 @@
+import os
+import tempfile
 from pecan import expose, response, redirect, conf, abort
 from webob.static import FileIter
+from prado.util import render, tar_czf
 
 
 class BuildController(object):
 
-    @expose(content_type='application/octet-stream', generic=True)
+    def __init__(self, name):
+        self.name = name
+
+    @expose(content_type='application/octet-stream')
     def index(self, **kw):
-        # TODO: grab the variables from `kw` and pass them to the templating
-        # engine to generate the right playbook variables
+        try:
+            conf.build_map[self.name]
+        except (AttributeError, KeyError):
+            abort(404)
+
+        template = conf.build_map[self.name]['template']
+        playbook = conf.build_map[self.name]['playbook']
+
+        files_to_compress = []
+        rendered_yml = render(template, **kw)
+        tmp_dir = tempfile.mkdtemp()
+        yml_file = os.path.join(tmp_dir, 'main.yml')
+        with open(yml_file, 'w') as f:
+            f.write(rendered_yml)
+        files_to_compress.append(playbook)
+        files_to_compress.append(yml_file)
+
+        playbook = tar_czf(files_to_compress)
+
         response.headers['Content-Disposition'] = 'attachment; filename=playbook.tar.gz'
-        response.headers['X-Accel-Redirect'] = 'playbook.tar.gz'
-        # FIXME: this needs to be programatic because we might get a lot of
-        # these given the different variables. Maybe a temporary location?
-        f = open(conf.playbook, 'rb')
+        f = open(playbook, 'rb')
         response.app_iter = FileIter(f)
 
+
+class BuildsController(object):
+
+    @expose()
+    def _lookup(self, name, *remainder):
+        return BuildController(name), remainder
