0bd398c3f5ee29a7daa37f872c433bbd239bca38
Alfredo Deza <alfredo@deza.pe>
2015-07-21 08:31:14 -0400

    create a better test config
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- prado/tests/config.py
+++ prado/tests/config.py
@@ -1,25 +1,62 @@
+from prado.hooks import error
+
 # Server Specific Configurations
 server = {
-    'port': '8080',
+    'port': '8000',
     'host': '0.0.0.0'
 }
 
 # Pecan Application Configurations
 app = {
-    'root': 'prado.controllers.root.RootController',
-    'modules': ['prado'],
-    'static_root': '%(confdir)s/../../public',
-    'template_path': '%(confdir)s/../templates',
-    'debug': True,
-    'errors': {
-        '404': '/error/404',
+    'root': '{{ app_name }}.controllers.root.RootController',
+    'modules': ['{{ app_name }}'],
+    'debug': False,
+    'hooks': [error.CustomErrorHook()],
+}
+
+logging = {
+    'loggers': {
+        'root': {'level': 'INFO', 'handlers': ['console']},
+        '{{ app_name }}': {'level': 'DEBUG', 'handlers': ['console']},
+        'pecan.commands.serve': {'level': 'DEBUG', 'handlers': ['console']},
+        'py.warnings': {'handlers': ['console']},
         '__force_dict__': True
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'color'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        },
+        'color': {
+            '()': 'pecan.log.ColorFormatter',
+            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
+                       '[%(threadName)s] %(message)s'),
+            '__force_dict__': True
+        }
     }
 }
 
-# Custom Configurations must be in Python dictionary format::
-#
-# foo = {'bar':'baz'}
-#
-# All configurations are accessible at::
-# pecan.conf
+# When True it will set the headers so that Nginx can serve the download
+# instead of Pecan.
+delegate_downloads = False
+
+build_map = {
+    "slave": {
+        "playbook": "%(confdir)s/../public/ceph-build/ansible",
+        "template": "%(confdir)s/../public/ceph-build/ansible/slave.yml.j2",
+        "command": 'ansible-playbook -i "localhost," -c local ../main.yml'
+    }
+}
+
+setup_ansible = '%(confdir)s/../public/ansible.tar.gz'
+
+# FIXME: we need a way to programatically know what IP/address we are serving
+# from
+service_address = 'http://172.18.181.11'
