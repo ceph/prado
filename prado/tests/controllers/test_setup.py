62031d98828cf652106aa18b3934e0adc9050b9f
Alfredo Deza <adeza@redhat.com>
2020-01-09 16:37:15 -0500

    tests: use bytestrings for body comparisons
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- prado/tests/controllers/test_setup.py
+++ prado/tests/controllers/test_setup.py
@@ -5,10 +5,12 @@ class TestSetupController(object):
     def test_configure_playbook(self, app):
         result = app.get('/setup/').json
         playbooks = result['playbooks']
-        assert playbooks == ['novars', 'slave', 'vars']
+        assert 'novars' in playbooks
+        assert 'slave' in playbooks
+        assert 'vars' in playbooks
 
     def test_get_ansible_tar(self, app):
-        result = app.get('/setup/ansible/', expect_errors=True)
+        result = app.get('/setup/ansible/', expect_errors=False)
         assert result.status_int == 200
 
     def test_get_ansible_tar_not_configured(self, app):
@@ -29,6 +31,6 @@ class TestSetupController(object):
         result = app.get('/setup/vars/?node_description=Ubuntu Precise Slave x86_64 Build Server&nodename=precise&labels=amd64 precise-pbuild x86_64&token=7479f92lff630c7436318580de6f3e27')
         assert result
 
-    def test_varargs_dunder_dunder(self, app):
+    def test_varargs_dunder_dunder(self, app, b_):
         result = app.get('/setup/vars/?node_description=Ubuntu Precise Slave x86_64 Build Server&nodename=precise__some-hash&labels=amd64 precise-pbuild x86_64&token=7479f92lff630c7436318580de6f3e27')
-        assert 'precise__some-hash' in result.body
+        assert b_('precise__some-hash') in result.body
